44903a7ed9eab117f8912058e52bbad9ceeb3e6d
nptwz <techxacm@gmail.com>
2013-07-21 10:34:40 +0800

    hw2

--- hw2.py
+++ hw2.py
@@ -2,6 +2,7 @@
 # Please fill out this stencil and submit using the provided submission script.
 
 from vec import Vec
+from GF2 import one
 
 
 
@@ -14,9 +15,9 @@ def vec_select(veclist, k):
     >>> v3 = Vec(D, {        'b': 2})
     >>> v4 = Vec(D, {'a': 10, 'b': 10})
     >>> vec_select([v1, v2, v3, v4], 'a') == [Vec(D,{'b': 1}), Vec(D,{'b': 2})]
-    True
-    '''
-    pass
+    True'''
+    return [ vec for vec in veclist if vec[k]==0 ]    
+
 
 def vec_sum(veclist, D): 
     '''
@@ -28,7 +29,7 @@ def vec_sum(veclist, D):
     >>> vec_sum([v1, v2, v3, v4], D) == Vec(D, {'b': 13, 'a': 11})
     True
     '''
-    pass
+    return sum(veclist,Vec(D,{}))
 
 def vec_select_sum(veclist, k, D): 
     '''
@@ -40,7 +41,7 @@ def vec_select_sum(veclist, k, D):
     >>> vec_select_sum([v1, v2, v3, v4], 'a', D) == Vec(D, {'b': 3})
     True
     '''
-    pass
+    return vec_sum(vec_select(veclist,k),D)
 
 
 
@@ -52,7 +53,7 @@ def scale_vecs(vecdict):
     >>> scale_vecs({3: v1, 5: v2}) == [Vec({1,2,3},{2: 3.0}), Vec({1,2,4},{1: 0.2, 2: 0.4, 4: 1.6})]
     True
     '''
-    pass
+    return [ 1.0/k*vecdict[k] for k in vecdict]
 
 
 
@@ -73,24 +74,29 @@ def GF2_span(D, L):
     >>> Vec(D, {x:one for x in D}) in GF2_span(D, L)
     True
     '''
-    pass
-
+    if len(L)==0:
+        return [ Vec(D,{}) ]
+    sub = GF2_span(D,L[1:])
+    subsub = [ item+L[0] for item in sub ]
+    return sub+subsub
+        
+    
 
 
 ## Problem 4
 # Answer with a boolean, please.
 
-is_it_a_vector_space_1 = ...
-is_it_a_vector_space_2 = ...
+is_it_a_vector_space_1 = True
+is_it_a_vector_space_2 = False
 
 
 
 ## Problem 5
-is_it_a_vector_space_3 = ...
-is_it_a_vector_space_4 = ...
+is_it_a_vector_space_3 = True
+is_it_a_vector_space_4 = False
 
 
 ## Problem 6
 
-is_it_a_vector_space_5 = ...
-is_it_a_vector_space_6 = ...
+is_it_a_vector_space_5 = True
+is_it_a_vector_space_6 = False
