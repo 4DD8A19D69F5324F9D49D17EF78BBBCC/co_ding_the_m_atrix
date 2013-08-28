58570600c32fb0f426b6ab62f14ddd7add7b2e34
programcaicai <techxacm@gmail.com>
2013-08-28 09:40:26 +0800

    hw7 finished

--- hw7.py
+++ hw7.py
@@ -6,7 +6,9 @@ import orthonormalization
 from mat import Mat
 from vec import Vec
 from vecutil import list2vec
-from matutil import listlist2mat
+from matutil import *
+from triangular import *
+import QR
 
 
 
@@ -109,7 +111,8 @@ least_squares_Q1 = listlist2mat([[.8,-0.099],[.6, 0.132],[0,0.986]])
 least_squares_R1 = listlist2mat([[10,2],[0,6.08]]) 
 least_squares_b1 = list2vec([10, 8, 6])
 
-x_hat_1 = ...
+
+x_hat_1 = triangular_solve_n(list(mat2rowdict(least_squares_R1).values()),least_squares_Q1.transpose()*least_squares_b1)
 
 
 least_squares_A2 = listlist2mat([[3, 1], [4, 1], [5, 1]])
@@ -117,7 +120,7 @@ least_squares_Q2 = listlist2mat([[.424, .808],[.566, .115],[.707, -.577]])
 least_squares_R2 = listlist2mat([[7.07, 1.7],[0,.346]])
 least_squares_b2 = list2vec([10,13,15])
 
-x_hat_2 = ...
+x_hat_2 = triangular_solve_n(list(mat2rowdict(least_squares_R2).values()),least_squares_Q2.transpose()*least_squares_b2)
 
 
 
@@ -139,5 +142,7 @@ def QR_solve(A, b):
         >>> result * result < 1E-10
         True
     '''
-    pass
+    Q, R = QR.factor(A)
+    Rlist = list(mat2rowdict(R).values())
+    return triangular_solve_n(Rlist,Q.transpose()*b)
 
